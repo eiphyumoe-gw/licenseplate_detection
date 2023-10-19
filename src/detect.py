7b0042cefb2dd0d2338f520cd7c0e823175e7bf6
eiphyumoe47 <EiPhyuMoe@globalwalkers.co.jp>
2023-10-19 09:10:12 +0630

    added json writer

--- src/detect.py
+++ src/detect.py
@@ -8,22 +8,23 @@ def make_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", default='/home/epm/licenseplate_detection/configs/detect.yaml', help="Path to your config file")
     parser.add_argument("--path", help= "Path to your image or video")
-    parser.add_argument("--output", default= '/home/epm/License_Plate_Detection/result', help= "Path to your ouput folder")
+    parser.add_argument("--output", default= '/home/epm/licenseplate_detection/result', help= "Path to your ouput folder")
     parser.add_argument("--name", default="yolox-s", help="Please select yolox-s or yolox-l")
 
     return parser
 
-def write_json(result_dict, output):
+def write_json(result_str, output):
     with open(output, "w") as outfile:
-        json.dump(result_dict, outfile)
+        json.dump(result_str, outfile)
 
 
 def main():
     args = make_parser().parse_args()
     configs = OmegaConf.load(args.config)
-    json_output = os.path.join(args.output, 'result.json')
+    json_output = os.path.join(args.output, 'predicted.json')
     demo = Demo(args, configs.YOLOX)
     result_str = demo.video_demo(args.path, args.output)
+    print("Output is ", json_output)
     write_json(result_str, json_output)
     
 
