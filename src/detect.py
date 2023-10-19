ea131c57eb2285947f535468e6452222273080b1
eiphyumoe47 <EiPhyuMoe@globalwalkers.co.jp>
2023-10-19 15:20:40 +0630

    modified configs

--- src/detect.py
+++ src/detect.py
@@ -1,14 +1,25 @@
+'''
+Filename: /home/epm/licenseplate_detection/src/detect.py
+Path: /home/epm/licenseplate_detection/src
+Created Date: Wednesday, October 18th 2023, 4:16:08 pm
+Author: EPM
+
+Copyright (c) 2023 GlobalWalkers,Inc. All rights reserved.
+
+'''
+
 import argparse
 from omegaconf import OmegaConf
 from utils import Demo
 import json
 import os
 
+
 def make_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", default='/home/epm/licenseplate_detection/configs/detect.yaml', help="Path to your config file")
+    parser.add_argument("--config", default='../licenseplate_detection/configs/detect.yaml', help="Path to your config file")
     parser.add_argument("--path", help= "Path to your image or video")
-    parser.add_argument("--output", default= '/home/epm/licenseplate_detection/result', help= "Path to your ouput folder")
+    parser.add_argument("--output", default= '../licenseplate_detection/result', help= "Path to your ouput folder")
     parser.add_argument("--name", default="yolox-s", help="Please select yolox-s or yolox-l")
 
     return parser
@@ -24,7 +35,6 @@ def main():
     json_output = os.path.join(args.output, 'predicted.json')
     demo = Demo(args, configs.YOLOX)
     result_str = demo.video_demo(args.path, args.output)
-    print("Output is ", json_output)
     write_json(result_str, json_output)
     
 
