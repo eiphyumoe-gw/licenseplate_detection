fc7ec1ffe0b81883a2cc2976eb15ff9ba768b99d
eiphyumoe47 <EiPhyuMoe@globalwalkers.co.jp>
2023-10-23 10:53:48 +0630

    modified

--- src/utils/demo.py
+++ src/utils/demo.py
@@ -18,7 +18,7 @@ class Demo:
         self.configs = configs
         self.load_model()
         self.IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
-        self.predictor = Predictor(self.model, self.exp, COCO_CLASSES, self.device, configs=self.configs)
+        self.predictor = Predictor(self.model, self.exp, device=self.device, configs=self.configs, args = self.args)
     
     def load_model(self):
         self.exp = get_exp(None, self.args.name) # select model name
